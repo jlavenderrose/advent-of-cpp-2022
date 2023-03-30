4394bd253c1f7dc9e4d96e209d9f331cde29ff6b
Julian Moore <jreedmoore@gmail.com>
2023-03-30 16:56:26 -0400

    Solve Day1 parts 1 and 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <chrono>
 
-using namespace std;
+using std::vector, std::string;
 
 int main()
 {
-   vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
 
-   for (const string& word : msg)
-   {
-      cout << word << " ";
-   }
-   cout << endl;
+    auto t1 = high_resolution_clock::now();
+    const string fname = "../data/day1.txt";
+    std::ifstream f{fname};
+
+    std::vector<int> calories = {};
+
+    if (!f.is_open()) {
+        std::cout << "Could not open file: " << fname << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int elf;
+    while (f) 
+    {
+        int entry = 0;
+        if (f.peek() == '\n') {
+            calories.push_back(elf);
+            std::push_heap(calories.begin(), calories.end());
+            elf = 0;
+            f.ignore(1, '\n');
+        } else {
+            f >> entry;
+            elf += entry;
+            f.ignore(1, '\n');
+        }
+    }
+
+    std::cout << "day2 part 1: " << calories.front() << std::endl;
+
+    int top_three = 0;
+    for (int i = 0; i<3; i++) {
+        std::pop_heap(calories.begin(), calories.end());
+        top_three += calories.back();
+        calories.pop_back();
+    }
+
+    std::cout << "day2 part 2: " << top_three << std::endl;
+    auto t2 = high_resolution_clock::now();
+    duration<double, std::milli> ms_double = t2 - t1;
+
+    std::cout << ms_double.count() << "ms" << std::endl;
+    return 0;
 }
